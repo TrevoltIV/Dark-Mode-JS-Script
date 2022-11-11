c4d09b15f8ee65186fba71fd6742a1c8dac89d16
TrevoltIV <113559351+TrevoltIV@users.noreply.github.com>
2022-11-11 18:25:15 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,15 @@
 
 
+//When using multiple link tags in document, either change the el variable or make sure the stylesheet link tag is at the top
 
 function screenMode(){
 	const el = document.querySelector("link");
-	const isDark = el.getAttribute("href");
-	const ifLight = isDark === "styles/index.css";
+	const styleSheet = el.getAttribute("href");
+	const ifLight = styleSheet === "styles/index.css";
 	ifLight ? el.href = "styles/index-darkmode.css" : el.href = "styles/index.css";
 }
+
+const button = document.getElementById("screenModeBtn");
+button.addEventListener("click", screenMode);
 
 
